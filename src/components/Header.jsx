a07cddc2b3b270d098f529d7ad19d41b07b62722
collins-kimotho <collinskimotho16@gmail.com>
2025-01-16 09:03:55 +0300

    styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,14 @@ import trollFace from "../images/troll-face.png"
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-start md:justify-center p-6 gap-4 bg-purple-700 font-inter">
+    <header className=" bg-purple-700 font-inter w-full overflow-hidden">
+      <div className="flex items-center justify-start md:justify-center p-6 gap-4">
         <img src={trollFace} alt="troll-face" width={50}
-            className=''
-        />
-        <h1 className="font-bold text-2xl text-white">Meme Generator</h1>
+              className=''
+          />
+          <h1 className="font-bold text-2xl text-white">Meme Generator</h1>
+        </div>
+       
     </header>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,13 +3,13 @@ import meme from '../images/takeMoney.webp'
 
 const Main = () => {
   return (
-    <main className="p-8 flex flex-col items-center justify-center m-auto">
-        <section className=''>
-            <div className="m-4  flex gap-4 w-full items-center justify-between">
-                <label htmlFor="" className="block">Top Text
+    <main className="flex items-center justify-center m-4 p-4 overflow-hidden">
+        <section className='flex flex-col items-center justify-center'>
+            <div className="flex items-center justify-center w-full gap-4">
+                <label htmlFor="" className="">Top Text
                     <input 
                         type="text" 
-                        className="border-2 block rounded-sm p-2 text-base"
+                        className="border-2 rounded-sm p-2 text-base w-fit block"
                         placeholder='One does not simply'
                         name='topText' 
                     />
@@ -17,14 +17,14 @@ const Main = () => {
                 <label htmlFor="" className="">Bottom Text
                     <input 
                         type="text " 
-                        className="border-2 block rounded-sm p-2 text-base"
+                        className="border-2 rounded-sm p-2 text-base block"
                         placeholder='Walk into Mordor'
                     />
 
                 </label>
-                
+               
             </div>
-            <button className="bg-purple-500 w-full mb-8 m-4 p-4 rounded-md">Get a new meme image</button>
+            <button className="bg-purple-500 w-full mb-8 m-4 p-4 rounded-md ">Get a new meme image</button>
             <div className="relative flex flex-col justify-center items-center mx-4 w-full">
                 <img src={meme} alt="" className=' rounded-sm m-4' />
                 <span className="absolute text-center top-0 text-4xl font-bold text-white p-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]
