f78180f1dbf8b57b6972e2b72a063d2fe5a9f213
collins-kimotho <collinskimotho16@gmail.com>
2025-01-16 13:23:38 +0300

    controlled components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import meme from '../images/takeMoney.webp'
+import { useState } from 'react'
 
 const Main = () => {
+
+    const [meme, setMeme] = useState({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageUrl: 'https://i.imgflip.com/1bij.jpg'
+    })
+
+    function handleChange(event) {
+        const {value, name} = event.currentTarget
+        setMeme(prevMeme => ({
+            ...prevMeme, [name]: value
+        }))
+    }
   return (
     <main className="flex items-center justify-center m-4 p-4 overflow-hidden">
         <section className='flex flex-col items-center justify-center'>
@@ -12,6 +26,8 @@ const Main = () => {
                         className="border-2 rounded-sm p-2 text-base w-fit block"
                         placeholder='One does not simply'
                         name='topText' 
+                        onChange={handleChange}
+                        value={meme.topText}
                     />
                 </label>
                 <label htmlFor="" className="">Bottom Text
@@ -19,6 +35,9 @@ const Main = () => {
                         type="text " 
                         className="border-2 rounded-sm p-2 text-base block"
                         placeholder='Walk into Mordor'
+                        name='bottomText'
+                        onChange={handleChange}
+                        value={meme.bottomText }
                     />
 
                 </label>
@@ -26,11 +45,12 @@ const Main = () => {
             </div>
             <button className="bg-purple-500 w-full mb-8 m-4 p-4 rounded-md ">Get a new meme image</button>
             <div className="relative flex flex-col justify-center items-center mx-4 w-full">
-                <img src={meme} alt="" className=' rounded-sm m-4' />
-                <span className="absolute text-center top-0 text-4xl font-bold text-white p-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]
-">One does not simply</span>
+                <img src={meme.imageUrl} alt="" className=' rounded-sm m-4' />
+                <span 
+                    className="absolute text-center top-0 text-4xl font-bold text-white p-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]
+                    ">{meme.topText.toUpperCase()}</span>
                 <span className="absolute text-center bottom-0 text-4xl font-bold text-white p-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]
-">Walk into Mordor</span>
+                    ">{meme.bottomText.toUpperCase()}</span>
             </div>
         </section>
        
