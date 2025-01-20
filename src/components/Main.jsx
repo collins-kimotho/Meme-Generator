234039d68eb5813fa7bb1b08e225b2554e27a19d
collins-kimotho <collinskimotho16@gmail.com>
2025-01-20 07:29:46 +0300

    use meme api to fetch images

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import meme from '../images/takeMoney.webp'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const Main = () => {
 
@@ -9,6 +9,7 @@ const Main = () => {
         bottomText: "Walk into Mordor",
         imageUrl: 'https://i.imgflip.com/1bij.jpg'
     })
+    const [allMemes, setAllMemes] = useState([])
 
     function handleChange(event) {
         const {value, name} = event.currentTarget
@@ -16,6 +17,23 @@ const Main = () => {
             ...prevMeme, [name]: value
         }))
     }
+
+    useEffect(()=>{
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
+
+    function getMemeImage(){
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const newMemeUrl = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            imageUrl: newMemeUrl
+        }))
+    }
+
+
   return (
     <main className="flex items-center justify-center m-4 p-4 overflow-hidden">
         <section className='flex flex-col items-center justify-center'>
@@ -43,11 +61,16 @@ const Main = () => {
                 </label>
                
             </div>
-            <button className="bg-purple-500 w-full mb-8 m-4 p-4 rounded-md ">Get a new meme image</button>
+            <button 
+                className="bg-purple-500 w-full mb-8 m-4 p-4 rounded-md "
+                onClick={getMemeImage}
+            >
+                Get a new meme image
+            </button>
             <div className="relative flex flex-col justify-center items-center mx-4 w-full">
                 <img src={meme.imageUrl} alt="" className=' rounded-sm m-4' />
                 <span 
-                    className="absolute text-center top-0 text-4xl font-bold text-white p-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]
+                    className="absolute text-center top-0 text-4xl font-bold text-white p-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] 
                     ">{meme.topText.toUpperCase()}</span>
                 <span className="absolute text-center bottom-0 text-4xl font-bold text-white p-4 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]
                     ">{meme.bottomText.toUpperCase()}</span>
